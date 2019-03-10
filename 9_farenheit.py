4dee087c521ad135112962c6313c140c216387cc
Mateus <pepitila@outlook.com>
2019-03-09 21:01:20 -0300

    Create 9_farenheit.py

---
+++ 9_farenheit.py
@@ -0,0 +1,5 @@
+far = float(input("Qual é a temperatura em graus Farenheit? "))
+
+cel = (5*(far - 32) / 9)
+
+print("A temperatura em graus Farenheit é de: {:.2f}°. E a sua correspondente em Celsius é de {:.2f}°".format(far,cel))
