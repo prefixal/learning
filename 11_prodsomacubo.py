63f4e68f344fd5bbdc0aa661da2cbb4768ed4527
Mateus <pepitila@outlook.com>
2019-03-09 21:22:32 -0300

    Create 11_prodsomacubo.py

---
+++ 11_prodsomacubo.py
@@ -0,0 +1,17 @@
+int1 = int(input("Digite o primeiro número inteiro: "))
+
+int2 = int(input("Digite o segundo número inteiro: "))
+
+real = float(input("Digite um número real: "))
+
+prod = (2*int1)*(int2)/2
+
+soma = 3*int1 + real
+
+cubo = real ** 3
+
+print("O produto dobro de {} com a metade de {} é: {} ".format(int1,int2,prod))
+
+print("A soma do triplo de {} com {} é de: {}".format(int1,real,soma))
+
+print("{} ELEVADO AO CUBO É: {}".format(real,cubo))
