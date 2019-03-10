d46f644d403525300b0356352ec79c14fba5fc21
Mateus <pepitila@outlook.com>
2019-03-09 21:05:16 -0300

    Create 10celsius.py

---
+++ 10celsius.py
@@ -0,0 +1,6 @@
+cel = float(input("Qual é a temperatura em graus Celsius? "))
+
+far = (9*cel/5) + 32 
+
+print("A temperatura em graus Celsius é de: {:.2f}°. E a sua correspondente em Farenheit é de {:.2f}°".format(cel,far))
+
