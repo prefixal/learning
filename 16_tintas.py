0bd140e9a6e03c5ac58c100b7481b2c0dbc19598
Mateus <pepitila@outlook.com>
2019-04-25 18:44:34 -0300

    Add files via upload

---
+++ 16_tintas.py
@@ -0,0 +1,4 @@
+area = float(input("Com uma lata de tinta pinta-se até 3 m² e custa R$ 80,00.\nDigite o tamanho da área a ser pintada em m²: "))
+print("Você deve comprar {:.2f} latas e isso custará R${:.2f}.".format(area/3, 80*(area/3)))
+ 
+
